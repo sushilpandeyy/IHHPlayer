770894adb9f75f698e910c3591c84d11700a3542
sushilpandeyy <contact.sushilpandey@gmail.com>
2023-10-08 09:55:35 +0530

    Changed Basis created Dataset

--- src/Scene/menu.jsx
+++ src/Scene/menu.jsx
@@ -10,17 +10,17 @@ const Menu = () => {
   <li>
   <Men
   icon="https://cdn.iconfinder.com/stored_data/1286809/128/png?token=1696134776-TNloRy9rDhtRMCW1GT3zlk%2FI6NPRbCTHik9YV5qUUic%3D"
-  title="HOME"
+  title="Home"
   /></li>
   <li>
   <Men
   icon="https://cdn.iconfinder.com/stored_data/1286809/128/png?token=1696134776-TNloRy9rDhtRMCW1GT3zlk%2FI6NPRbCTHik9YV5qUUic%3D"
-  title="HOME"
+  title="Search"
   /></li>
   <li>
   <Men
   icon="https://cdn.iconfinder.com/stored_data/1286809/128/png?token=1696134776-TNloRy9rDhtRMCW1GT3zlk%2FI6NPRbCTHik9YV5qUUic%3D"
-  title="HOME"
+  title="Genre"
   /></li>
   </ul>
   </Stack>
