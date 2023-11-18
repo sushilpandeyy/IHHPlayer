0a9aa7661c92a785a349220ad22a5e4a687ff405
Contactsushil <contact.sushilpandey@gmail.com>
2023-11-18 15:22:47 +0530

    Inline scroll Built

--- Data/newalb.js
+++ Data/newalb.js
@@ -22,6 +22,11 @@ const artist = [{
     key: "artist_05",
     img: "https://ihhplayer.s3.ap-south-1.amazonaws.com/Artist/Theprophc.jpeg",
     name: "The PropheC"
+},
+{
+    key: "artist_06",
+    img: "https://ihhplayer.s3.ap-south-1.amazonaws.com/PANTHER.webp",
+    name: "Panther"
 }
 ]
 const all = [
@@ -31,6 +36,7 @@ const all = [
         title: "Blowing Up",
         artist: "KR$NA",
         genre: "Delhi Scene",
+        key: "AwhyFo5N0cg",
         album: "",
     },
     {
@@ -39,6 +45,7 @@ const all = [
         title: "Badman Ft WickedSunnyy",
         artist: "SuperManikk",
         genre: "Delhi Scene",
+        key: "hsYblpKpJF8",
         album: "",
     },
     {
@@ -46,6 +53,7 @@ const all = [
         img: "https://i3.ytimg.com/vi/q_D3K1mnXXM/maxresdefault.jpg",
         title: "KING OF INDIAN HIP HOP (PROD BY Babz beats)",
         artist: "Emiway Bantai",
+        key: "q_D3K1mnXXM",
         genre: "Mumbai Scene",
         album: "King Of The Streets",
     },
@@ -53,6 +61,7 @@ const all = [
         src: "https://ihhplayer.s3.ap-south-1.amazonaws.com/Brand+New++Seedhe+Maut.mp3",
         img: "https://i.ytimg.com/vi/7pNo-geT_MA/0.jpg",
         title: "Brand New",
+        key: "7pNo-geT_MA",
         artist: "Seedhe Maut",
         genre: "Delhi Scene",
         album: "LunchBreak",
@@ -61,6 +70,7 @@ const all = [
         src: "https://ihhplayer.s3.ap-south-1.amazonaws.com/Shiv+Kumar+Batalvi+-+Ki+Pushde+o+haal+Fakiran+da+(Prod.+By+Wazir+Patar).mp3",
         img: "https://i.ytimg.com/vi/jEfUeVlauJ4/0.jpg",
         title: "Ki Pushde o haal Fakiran da",
+        key: "jEfUeVlauJ4",
         artist: "Shiv Kumar Batalviya",
         genre: "Poetry",
         album: "",
@@ -68,10 +78,20 @@ const all = [
     {
         src: "https://ihhplayer.s3.ap-south-1.amazonaws.com/MERE+BINA++%40KSHMRmusic++The+PropheC++%40TalhaAnjum+++Official+Audio++%23Karam.mp3",
         img: "https://i.ytimg.com/vi/jcPC7AHQMKs/0.jpg",
+        key: "jcPC7AHQMKs",
         title: "Mere Bina @KSHMRmusic The PropheC @TalhaAnjum",
         artist: "The PropheC",
         genre: "Lyrical",
         album: "Karam",
+    },
+    {
+        src: "https://ihhplayer.s3.ap-south-1.amazonaws.com/Galat+Karam.mp3",
+        img: "https://i.ytimg.com/vi/qot0iPHozdk/0.jpg",
+        key: "qot0iPHozdk",
+        title: "Panther X Raga - Galat Karam",
+        artist: "Panther",
+        genre: "Rage",
+        album: "",
     },
 ]
 
--- src/Component/Cardart.jsx
+++ src/Component/Cardart.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Cardart = (props) => {
-    console.log(props)
+    
   return (
     <>
-        <div className="card-outer m-2 drop-shadow-2xl place-items-start overflow-hidden h-200 w-240">
-            <img className='art-img rounded-lg ' src={props.src} />
+        <div className="card-art drop-shadow-2xl place-items-start">
+            <img className='rounded-lg card-img-art' src={props.src} />
             <h3>{props.title}</h3>
         </div>
     </>
