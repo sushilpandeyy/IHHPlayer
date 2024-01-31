b1b9599c31bf580b02557aa51598d4ad469eacc2
Contactsushil <contact.sushilpandey@gmail.com>
2024-01-31 16:21:32 +0530

    Mobile Grid

--- src/Scene/Artist.jsx
+++ src/Scene/Artist.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {artist} from "../../Data/newalb.js"
 import Cardart from '../Component/Cardart'
+import './Scene.css'
+
 function artcreate(item){
   return <Cardart 
   key={item.key}
@@ -11,7 +13,7 @@ function artcreate(item){
 const Artist = () => {
   return (
     <>
-    <div className="grid grid-cols-5 gap-5">
+    <div className="grid grid-cols-5 gap-5 mobilecards">
     {artist.map(artcreate)}
     </div>
     </>
