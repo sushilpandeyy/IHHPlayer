2555eabfac01dacc67c2dc98e8be1ebaf0002114
Contactsushil <contact.sushilpandey@gmail.com>
2024-03-29 03:12:46 +0530

    Added Signup Wiring

--- Client/src/Scene/Login.jsx
+++ Client/src/Scene/Login.jsx
@@ -121,6 +121,47 @@ const Loginform = ({ SetSig, Sig }) => {
   };
 
 const SignupForm = ({ SetSig, Sig }) => {
+    const [Message, setm] = useState("");
+    const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      password: ''
+    });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value
+        });
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          const response = await fetch('https://api.contactsushil.me/add', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+          });
+    
+          if (response.ok) {
+              setm("Account Created")
+              const userProfileData = await response.json();
+              setCookie('userID', userProfileData.user_id, 30);
+              setCookie('Name', userProfileData.name, 30);
+              navigate('/');
+              
+          } else {
+              setm("Account Creation Fail")
+          }
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+    
     return (
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -136,19 +177,41 @@ const SignupForm = ({ SetSig, Sig }) => {
               <form className="space-y-4 md:space-y-6" action="#">
               <div>
                   <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
-                  <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Prem Prakash" required="" />
+                  <input 
+                      type="text" 
+                      name="name" 
+                      id="name" 
+                      value={formData.name}
+                      onChange={handleChange}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Prem Prakash" required="" />
                 </div>
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                  <input 
+                  type="email" 
+                  name="email" 
+                  id="email" 
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
                 </div>
                 <div>
                   <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  <input 
+                  type="password" 
+                  name="password" 
+                  id="password" 
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="••••••••" 
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                 </div>
                 <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Already have an account? <span href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500" onClick={() => { SetSig(true) }}>Sign in</span>
+                </p>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <span className="font-medium text-primary-600 hover:underline dark:text-primary-500">{Message}</span>
                 </p>
               </form>
             </div>
