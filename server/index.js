3fb6eada5761c909a3cc7d8aa3e626181088c31c
Contactsushil <contact.sushilpandey@gmail.com>
2024-03-29 14:23:25 +0530

    Added Artist Input Method

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getMusic, getsample } from "./Apis/getapis.js";
-import { adduser, checkuser } from "./Apis/postapis.js";
+import { addartist, adduser, checkuser } from "./Apis/postapis.js";
 import fs from 'fs'
 import https from 'https';
 import cors from 'cors';
@@ -20,12 +20,16 @@ async function startServer() {
         app.use(express.json());
         app.get('/Music', getMusic);
         app.post('/login', checkuser);
+        app.post('/addartist', addartist);
         app.post('/add', adduser);
         app.get('/', getsample);
         const server = https.createServer(options, app);
         server.listen(443, () => {
             console.log('Server is running on port 443');
         });
+       // app.listen(3000, ()=> {
+       //     console.log("Running at 3000");
+       // })
     } catch (error) {
         console.error("Failed to start server:", error);
     }
