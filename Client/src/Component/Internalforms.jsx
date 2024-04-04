4351c86ef4ec2c586cb92692acbda871d0dc643d
Contactsushil <contact.sushilpandey@gmail.com>
2024-04-04 22:36:34 +0530

    API rectifications

--- Client/src/Component/Internalforms.jsx
+++ Client/src/Component/Internalforms.jsx
@@ -108,7 +108,7 @@ export const Artistform = () => {
 
 async function fetchArtist() {
   try {
-    const response = await fetch('http://localhost:3000/artist');
+    const response = await fetch('https://api.contactsushil.me//artist');
     if (!response.ok) {
       throw new Error('Failed to fetch artist data');
     }
@@ -218,7 +218,7 @@ export const MusicForm = () => {
 
         console.log(updatedFormData);
 
-        const response = await fetch('http://localhost:3000/addmusic', {
+        const response = await fetch('https://api.contactsushil.me//addmusic', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
