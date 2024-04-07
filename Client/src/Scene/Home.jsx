b1dbba7b5b1931d40d38139c5eb7dcf3f991ac25
Contactsushil <contact.sushilpandey@gmail.com>
2024-04-07 11:41:32 +0530

    Added Help to Fascia

--- Client/src/Scene/Home.jsx
+++ Client/src/Scene/Home.jsx
@@ -65,10 +65,18 @@ const Home = () => {
         console.error('Error fetching data:', error);
       }
     };
+    const startfascia = async () => {
+      try {
+        const response = await axios.get('https://fascia-backend.onrender.com/');
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
     fetchData();
     fetchDatadelhi();
     fetchArtist();
+    startfascia();
   }, []);
   function removeDuplicateObjects(array, key) {
     return array.filter((obj, index, self) => 
